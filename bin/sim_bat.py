9c13766639814e6f86e25ae5417057508b27fbb3
Michael Vogt <mvo@ubuntu.com>
2022-10-03 10:22:34 +0200

    bin: improve date formating

--- bin/sim_bat.py
+++ bin/sim_bat.py
@@ -3,26 +3,39 @@
 import datetime
 import sys
 
+import matplotlib.ticker
+import matplotlib.dates
 
 from energymon.db import EnergyDatabase
 
 
+class RealConciseFormatter(matplotlib.ticker.Formatter):
+    def __init__(self):
+        self._last_day = datetime.datetime.fromtimestamp(0)
+    def __call__(self, x, pos=0):
+        dt = matplotlib.dates.num2date(x)
+        if dt.date() != self._last_day.date():
+            self._last_day = dt
+            return dt.strftime("%Y-%m-%d")
+        return dt.strftime("%H:%M")
+
+
 class BatterySimulator:
-    def __init__(self, dbpath, capacity=5*1000, cost_per_kw=0.35):
+    # cost_per_kwh=0.35€-0.10€
+    def __init__(self, dbpath, capacity=5*1000, cost_per_kwh=0.25):
         self._db = EnergyDatabase(dbpath)
         # XXX: assume battery is full at the begnning for easier stats
-        self._bat = capacity
+        self._bat = capacity/2
         self._cap = capacity
-        self._cost_per_kw = cost_per_kw
+        self._cost_per_kwh = cost_per_kwh
         self._energy_from_battery = 0
         self._energy_from_grid = 0
-        # these stats are a bit useless
-        self._n_full = 0
-        self._n_empty = 0
-    def simulate(self, ):
+        self._energy_to_grid = 0
+    def simulate(self):
         cur = self._db._get_all_current_power()
         first_dt = last_dt = datetime.datetime.fromtimestamp(cur.fetchone()[0])
-        bat_empty_or_full = False
+        bat_cap = []
+        time = []
         for row in cur:
             ts, energy = row[0], row[1]
             dt = datetime.datetime.fromtimestamp(ts)
@@ -35,11 +48,7 @@ class BatterySimulator:
                 if self._bat > energy_per_time:
                     self._energy_from_battery += energy_per_time
                     self._bat -= energy_per_time
-                    bat_empty_or_full = False
                 else:
-                    if not bat_empty_or_full:
-                        self._n_empty += 1
-                        bat_empty_or_full = True
                     self._energy_from_grid += energy_per_time
             # energy availabe, store
             if energy_per_time < 0:
@@ -47,24 +56,37 @@ class BatterySimulator:
                     self._bat += -1*energy_per_time
                     bat_empty_or_full = False
                 else:
-                    if not bat_empty_or_full:
-                        self._n_full += 1
-                        bat_empty_or_full = True
+                    self._energy_to_grid += energy_per_time
+            time.append(dt)
+            bat_cap.append(self._bat)
             last_dt = dt
         total_time = last_dt - first_dt
         bat_kw = self._energy_from_battery/1000
         # fugly, return sensible data instead
         print(f"Total {bat_kw:.2f} kWh taken from battery in {total_time}")
         print(f"Total taken from grid {self._energy_from_grid/1000:.2f} kWh")
-        money = bat_kw*self._cost_per_kw
+        print(f"Total given to grid {self._energy_to_grid/1000:.2f} kWh")
+        money = bat_kw*self._cost_per_kwh
         print(f"Money {money:.2f}€")
-        print(f"Total full: {self._n_full} empty: {self._n_empty}")
+        return time, bat_cap
 
 
 def main():
     dbpath = sys.argv[1]
     bat = BatterySimulator(dbpath)
-    bat.simulate()
+    dates, energy = bat.simulate()
+
+    import matplotlib.dates
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots()
+    fig.set_figwidth(200)
+    ax.plot(dates, energy)
+    loc = matplotlib.dates.HourLocator(interval=2)
+    ax.xaxis.set_major_locator(loc)
+    fmt = RealConciseFormatter()
+    ax.xaxis.set_major_formatter(fmt)
+    fig.autofmt_xdate()
+    plt.show()
 
 
 if __name__ == "__main__":
